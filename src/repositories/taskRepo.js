ae3e819aedcd88259ee43410ec0a0f4956ea0f18
Mujtaba Ali Khan <mujtaba6159@gmail.com>
2026-04-14 21:47:11 -0400

    Add completed query filtering feature

--- src/repositories/taskRepo.js
+++ src/repositories/taskRepo.js
@@ -4,6 +4,15 @@ export async function findAll() {
   return prisma.task.findMany();
 }
 
+// Making a new function for filtering........
+export async function findByCompleted(completed) {
+  return prisma.task.findMany({
+    where: {
+      completed: completed
+    }
+  });
+}
+
 // Create a new task
 export async function create(data) {
   return prisma.task.create({
--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,7 +1,21 @@
 import * as taskRepository from '../repositories/taskRepo.js';
 
-export async function getAllTasks() {
-  return taskRepository.findAll();
+export async function getAllTasks(completed) {
+  // I am making sure no query return all tasks
+  if (completed === undefined) {
+    return taskRepository.findAll();
+  }
+
+
+  if (completed !== 'true' && completed !== 'false') {
+    throw new Error('Invalid completed query parameter');
+  }
+
+
+  const isCompleted = completed === 'true';
+
+
+  return taskRepository.findByCompleted(isCompleted);
 }
 
 export async function createTask(newTask) {
